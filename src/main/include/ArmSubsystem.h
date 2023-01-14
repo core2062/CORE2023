b3bd222307933912a82a6c99196260d35367c195
Ictoan53 <Ictoan53@gmail.com>
2023-01-14 15:43:49 -0600

    Wrote Arm Subsystem

---
+++ src/main/include/ArmSubsystem.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ctre/Phoenix.h>
+#include <CORERobotLib.h>
+#include "Config.h"
+
+using namespace CORE;
+using namespace frc;
+
+class ArmSubsystem : public CORESubsystem {
+public:
+    ArmSubsystem();
+    void RobotInit() override;
+    void TeleopInit() override;
+    void Teleop() override;
+
+    void SetArmMotorSpeed(double percent);
+    double GetArmMotorPosition();
+    void ResetEncoders();
+    void InitTalons();
+
+private:
+    COREConstant<double> m_armMotorSpeedModifier;
+    TalonSRX m_armMotor;
+    
+};
