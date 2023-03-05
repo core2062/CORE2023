b8cbfd90c4aff6c3c4807c2f3a5c9b8d68d39bb5
Ictoan53 <Ictoan53@gmail.com>
2023-03-05 15:20:18 -0600

    Fixed autobalance

--- src/main/cpp/DriveSubsystem.cpp
+++ src/main/cpp/DriveSubsystem.cpp
@@ -11,8 +11,8 @@ DriveSubsystem::DriveSubsystem() :
 		m_etherQuickTurnValue("Ether Quick Turn Value", .5),
         m_ticksPerInch("Ticks Per Inch", (4 * 3.1415) / 1024),
 		m_balanceMaxSpeed("Max speed of Balance", 0.75),
-		m_balanceCalibration("Robot Pitch", 2.5),
-		m_balanceMaxCalibration("max proportion of robot", 0.50),
+		m_balanceCalibration("Robot Pitch", -2.56),
+		m_balanceMaxCalibration("max proportion of robot", 0.4),
 		m_driveSpeedModifier("Drive speed Modifier", 0.75),
 		m_driveSpeedModifierSlow("Drive speed Modifier slowed", 0.25),
 		m_compressor(frc::PneumaticsModuleType::REVPH) {
@@ -139,7 +139,7 @@ void DriveSubsystem::SetTalonMode(NeutralMode mode){
 
 
 void DriveSubsystem::Balance(){
-	m_currentPitch = -ahrs.GetRoll() + m_balanceCalibration.Get(); 
+	m_currentPitch = ahrs.GetPitch() + m_balanceCalibration.Get(); 
 	std::cout << "the pitch is: " << m_currentPitch << endl;
 	if (abs(m_currentPitch) >= 0.7){
 	double Proportion = m_currentPitch/15.0;
